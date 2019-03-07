fc2d43caf59170a3638ec4185d6e604a1adc6b47
Pujan Soni <pujansoni1997@gmail.com>
2019-03-08 00:15:10 +0530

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :lockable, :zxcvbnable
+
+  def send_devise_notification(notification, *args)
+    devise_mailer.send(notification, self, *args).deliver_later
+  end
+end
